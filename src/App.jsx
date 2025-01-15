845349a97e16d324f10cdb9a096d0616a06a09ef
Aashin Mohammed A Z <ashinmammu@gmail.com>
2025-01-15 20:50:09 +0530

    created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+
+function App() {
+  
+
+  return (
+    <main className='bg-black'>
+    <Navbar />
+    <Hero />
+    </main>
+  )
+}
+
+export default App
